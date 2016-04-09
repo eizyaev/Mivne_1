aeb4f5c97dc15122cb5acea9f4fd10d1fd3e98e5
AmeerHajAli <AmeerHajAli>
2016-04-09 19:38:13 +0300

    added stages and exe

--- sim_core.c
+++ sim_core.c
@@ -39,19 +39,20 @@ int SIM_CoreReset(void)
         Core.pipeStageState[i].src1Val = 0;
         Core.pipeStageState[i].src2Val = 0;
     }
-    for ( i = 0 ; i < SIM_REGFILE_SIZE ; i++)
+    for ( i = 0 ; i < SIM_REGFILE_SIZE ; i++){
+
         Core.regFile[i] = 0;
-     
-        fetch_cur.cmd = Core.pipeStageState[0].cmd;
-        fetch_next.cmd = Core.pipeStageState[0].cmd;
-        dec_cur.cmd = Core.pipeStageState[1].cmd;
-        dec_next.cmd = Core.pipeStageState[1].cmd;
-        exe_cur.cmd = Core.pipeStageState[2].cmd;
-        exe_next.cmd = Core.pipeStageState[2].cmd;
-        mem_cur.cmd = Core.pipeStageState[3].cmd;
-        mem_next.cmd = Core.pipeStageState[3].cmd;
-        wb_cur.cmd = Core.pipeStageState[4].cmd;
-        wb_next.cmd = Core.pipeStageState[4].cmd;
+    }        
+    fetch_cur.cmd = Core.pipeStageState[0].cmd;
+    fetch_next.cmd = Core.pipeStageState[0].cmd;
+    dec_cur.cmd = Core.pipeStageState[1].cmd;
+    dec_next.cmd = Core.pipeStageState[1].cmd;
+    exe_cur.cmd = Core.pipeStageState[2].cmd;
+    exe_next.cmd = Core.pipeStageState[2].cmd;
+    mem_cur.cmd = Core.pipeStageState[3].cmd;
+    mem_next.cmd = Core.pipeStageState[3].cmd;
+    wb_cur.cmd = Core.pipeStageState[4].cmd;
+    wb_next.cmd = Core.pipeStageState[4].cmd;
 
     return 0;
 }
@@ -89,20 +90,39 @@ void SIM_CoreGetState(SIM_coreState *curState)
 void pipestage_fetch(void)
 {
     SIM_MemInstRead(Core.pc, &fetch_next.cmd);
-    dec_next.cmd = fetch_cur.cmd;
+    dec_next.cmd = fetch_cur.cmd;// TODO FORWARDING / BRANCH HAZARD
     Core.pc += 4;
     fetch_cur = fetch_next;
 }
 
 void pipestage_dec(void)
 {
-    exe_next.cmd = dec_cur.cmd;
+    exe_next.cmd = dec_cur.cmd;// TODO FORWARDING / BRANCH HAZARD
     dec_cur = dec_next;
 }
 
 void pipestage_exe(void)
 {
-    mem_next.cmd = exe_cur.cmd;
+    mem_next.cmd = exe_cur.cmd;// TODO FORWARDING / BRANCH HAZARD
+    switch (exe_cur.cmd.opcode)
+    {
+    case 0: // TODO {"NOP", "ADD", "SUB", "LOAD", "STORE", "BR", "BREQ", "BRNEQ" }
+        break;
+    case 1:
+    	break;
+    case 2:
+        break;
+    case 3:
+    	break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+    	break;
+    case 7:
+        break;
+    }
     exe_cur = exe_next;
 }
 
@@ -118,7 +138,7 @@ void pipestage_wb(void)
 }
 
 void UpdateCoreState(void)
-{
+{// VERY GOOD, FORWARDING WILL BE IMPLEMENTED IN OTHER FUNCTIONS
     Core.pipeStageState[0].cmd = fetch_cur.cmd;
     Core.pipeStageState[1].cmd = dec_cur.cmd;
     Core.pipeStageState[2].cmd = exe_cur.cmd;
