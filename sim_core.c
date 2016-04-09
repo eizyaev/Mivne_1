6950a08c0768f894027c8f6175f2f8cf3508412f
eizyaev <eivizyaev@gmail.com>
2016-04-09 13:20:04 +0300

    Added SIM_CoreReset()

--- sim_core.c
+++ sim_core.c
@@ -2,10 +2,29 @@
 /* This file should hold your implementation of the CPU pipeline core simulator */
 
 #include "sim_api.h"
+SIM_coreState Core;
 
-int SIM_CoreReset(void)
+
+int SIM_CoreReset()
 {
-    return -1;
+    int i;
+    Core.pc = 0;
+
+    for ( i = 0 ; i < SIM_PIPELINE_DEPTH ; i++)
+    {
+        Core.pipeStageState[i].cmd.opcode = 0; 
+        Core.pipeStageState[i].cmd.src1 = 0; 
+        Core.pipeStageState[i].cmd.src2 = 0;
+        Core.pipeStageState[i].cmd.isSrc2Imm = false; 
+        Core.pipeStageState[i].cmd.dst = 0;
+        Core.pipeStageState[i].src1Val = 0;
+        Core.pipeStageState[i].src2Val = 0;
+    }
+    for ( i = 0 ; i < SIM_REGFILE_SIZE ; i++)
+        Core.regFile[i] = 0;
+    
+
+    return 0;
 }
 
 void SIM_CoreClkTick(void)
